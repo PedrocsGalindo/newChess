118005bbc366a8c5f685afa0ad4a9e6cb13416e8
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-06-29 16:41:26 -0300

    feat: getPeca(int, char)

--- app_chess/src/models/Tabuleiro.java
+++ app_chess/src/models/Tabuleiro.java
@@ -10,11 +10,18 @@ public class Tabuleiro {
         return(linha, coluna)
     }
     public Peca getPeca(Posicao pos) {
-        linha = pos.getLinha().getNumero()
-        coluna = pos.getColuna().getNumero()
-        casa = this.casas[linha][coluna]
-        peca = casa.getPeca
-        return peca
+        linha = pos.getLinha().getNumero();
+        coluna = pos.getColuna().getNumero();
+        casa = this.casas[linha][coluna];
+        peca = casa.getPeca;
+        return peca;
+    }
+    public Peca getPeca(int linha, char coluna) {
+        coluna = Character.toLowerCase(coluna);
+        coluna = (letra - 'a') + 1;
+        casa = this.casas[linha][coluna];
+        peca = casa.getPeca;
+        return peca;
     }
     public void moverPeca(Posicao origem, Posicao destino) { ... }
     public void inicializar() { ... }
