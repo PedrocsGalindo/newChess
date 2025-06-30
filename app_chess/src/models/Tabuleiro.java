f775ec749fd28f8869b65e40248461b8ce983f6b
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-06-30 20:41:01 -0300

    feat: contructor

--- app_chess/src/models/Tabuleiro.java
+++ app_chess/src/models/Tabuleiro.java
@@ -1,20 +1,36 @@
 public class Tabuleiro {
     private Casa[][] casas = new Casa[8][8];
 
-    public Peca getPeca(Posicao pos) {
+    // Tabuleiro vazio
+    public Tabuleiro(){
+        int i = 1;
+        for (Linha l : Linha.values()){
+            int j = 1;
+            for (Coluna c : Coluna.values()){
+                this.casas[i][j] = new Casa(new Posicao(l, c));
+                j ++;
+            }
+            i++;
+        }
+    }
+    
+    public Casa getCasa(Posicao pos) {
         int linha = pos.getLinha().getNumero();
         int coluna = pos.getColuna().getNumero();
-        Casa casa = this.casas[linha][coluna];
-        Peca peca = casa.getPeca();
-        return peca;
+        return this.casas[linha-1][coluna-1];       // -1 because index 0-7 and chess 1-8
     }
-    public Peca getPeca(int linha, char coluna) {
-        coluna = Character.toLowerCase(coluna);
-        int n_coluna = (coluna - 'a') + 1;
-        Casa casa = this.casas[linha][n_coluna];
-        Peca peca = casa.getPeca();
-        return peca;
+    public void moverPeca(Posicao origem, Posicao destino) {
+        getCasa(destino).setPeca(getCasa(origem).getPeca());
+        getCasa(origem).setPeca(null);
     }
-    public void moverPeca(Posicao origem, Posicao destino) { ... }
-    public void inicializar() { ... }
+    public void inicializar() {
+        Cor cor_preta = Cor.PRETO;
+        Cor cor_branca = Cor.BRANCO;
+
+        this.casas[0][0].setPeca(new Torre(cor_branca));
+        this.casas[0][8].setPeca(new Torre(cor_branca));
+
+        this.casas[8][0].setPeca(new Torre(cor_preta));
+        this.casas[8][8].setPeca(new Torre(cor_preta));
+    }
 }
