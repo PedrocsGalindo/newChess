c7dbf568c554afabe89d10c718f4905481b1fb7c
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-07-15 11:34:48 -0300

    feat/ Exceptino

---
+++ ChessRules/src/main/java/chessRules/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package chessRules.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body("Erro: " + ex.getMessage());
+    }
+}
