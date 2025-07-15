d3de5d1b7846d0743f4da7c470de1380d83573e5
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-07-15 10:23:41 -0300

    feat/chessGame structure

---
+++ ChessGame/src/main/java/chessGame/models/ChessGameApplications.java
@@ -0,0 +1,12 @@
+package chessRules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessGameApplications {
+
+  public static void main(String... args) {
+    SpringApplication.run(ChessGameApplications.class, args);
+  }
+}
