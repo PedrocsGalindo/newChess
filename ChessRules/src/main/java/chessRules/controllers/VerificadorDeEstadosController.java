7c5ede46afab4c61c968f7fce74c932d949e80fb
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-07-15 11:35:15 -0300

    feat/ verificar estado service

--- ChessRules/src/main/java/chessRules/controllers/VerificadorDeEstadosController.java
+++ ChessRules/src/main/java/chessRules/controllers/VerificadorDeEstadosController.java
@@ -1,5 +1,30 @@
 package chessRules.controllers;
 
+import chessRules.dtos.JogadaPossiveisRequest;
+import chessRules.utils.VerificadorDeEstados;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class VerificadorDeEstadosController {
-    
+
+    private final VerificadorDeEstados ve = new VerificadorDeEstados();
+
+    @GetMapping("/ChesseRules/verificarEstado")
+    String all(@RequestBody JogadaPossiveisRequest request) {
+        /*
+        Exemplo of request:
+            {
+                "Tabuleiro": ["WR1a", "WN2a","WB3a", ...]
+                },
+                "Cor": "BRANCO"
+            }
+
+        Exemplo of return:
+            {"EM ANDAMENTO"}
+        */
+        return ve.verificarEstado(request.getTabuleiro(), request.getCor());
+    }
 }
