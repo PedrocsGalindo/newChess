174f5d0634268cbb6c6cb6bb6ae923961494bd32
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-07-25 20:48:04 -0300

    feat/dtos

---
+++ ChessRules/src/main/java/chessRules/dtos/requests/JogadasPossiveisRequest.java
@@ -0,0 +1,23 @@
+package chessRules.dtos.requests;
+
+import chessRules.models.Posicao;
+import chessRules.models.Tabuleiro;
+
+import java.util.List;
+
+public class JogadasPossiveisRequest {
+    private List<String> tabuleiro;
+    private String posicao;
+
+    public JogadasPossiveisRequest(){
+
+    }
+    public Tabuleiro getTabuleiro() {
+        return new Tabuleiro(this.tabuleiro);
+    }
+    public Posicao getPosicao() {
+        int l = Character.getNumericValue(posicao.charAt(0));
+        char c = posicao.charAt(1);
+        return new Posicao(l, c);
+    }
+}
